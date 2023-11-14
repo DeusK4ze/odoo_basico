fd0c850810ef14f06fd6f9f809a3c28e4a021c72
a22hugorp <hugopaternal@gmail.com>
2023-11-14 14:26:50 +0100

    incluido suceso.py pero odoo da error asi que seguiremos las instrucciones de antonio

--- models/informacion.py
+++ models/informacion.py
@@ -7,12 +7,12 @@ class informacion(models.Model):
     _name = 'odoo_basico.informacion'
     _description = 'Este es el modulo'
     name = fields.Char(required=True, size=20, string='Título')
-    description = fields.Text(string='A descripción: ')
+    descripcion = fields.Text(string='A descripción: ')
     alto_en_cms=fields.Integer(string="Alto en centímetros")
     longo_en_cms = fields.Integer(string="Longo en centímetros")
     ancho_en_cms = fields.Integer(string="Ancho en centímetros")
     peso = fields.Float(digits=(6,2), default=2.7, string="Peso en Kg")
     autorizado = fields.Boolean(string="¿Autorizado?", default=True)
-    sexo_traducido = fields.Selection([('hombre', 'Hombre'),('mujer', 'Mujer'),('Otros', 'Outros')], string='Sexo',required=True)
+    sexo_traducido = fields.Selection([('Hombre', 'Home'),('Mujer', 'Muller'),('Otros', 'Outros')], string='Sexo',required=True)
 
 
---
+++ models/suceso.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class suceso(models.Model):
+    _name = 'odoo_basico.informacion'
+    _description = 'Este es el modulo'
+    name = fields.Char(required=True, size=20, string="Suceso")
+    descripcion = fields.Text(string="A Descripción do Suceso")  # string é a etiqueta do campo
+    nivel = fields.Selection([('Baixo', 'Baixo'), ('Medio', 'Medio'), ('Alto', 'Alto')], string='Nivel')
