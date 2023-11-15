d7e0e48c1dedcb91d30a5d5b424819c1cbfaf143
a22hugorp <hugopaternal@gmail.com>
2023-11-15 14:28:16 +0100

    grupos hechos y separados

--- models/informacion.py
+++ models/informacion.py
@@ -8,11 +8,25 @@ class informacion(models.Model):
     _description = 'Este es el modulo'
     name = fields.Char(required=True, size=20, string='Título')
     descripcion = fields.Text(string='A descripción: ')
-    alto_en_cms=fields.Integer(string="Alto en centímetros")
+    alto_en_cms = fields.Integer(string="Alto en centímetros")
     longo_en_cms = fields.Integer(string="Longo en centímetros")
     ancho_en_cms = fields.Integer(string="Ancho en centímetros")
-    peso = fields.Float(digits=(6,2), default=2.7, string="Peso en Kg")
+    volume = fields.Float(compute="_volume", store=True)
+    literal = fields.Char(store=False)
+    peso = fields.Float(digits=(6, 2), default=2.7, string="Peso en Kg")
     autorizado = fields.Boolean(string="¿Autorizado?", default=True)
-    sexo_traducido = fields.Selection([('Hombre', 'Home'),('Mujer', 'Muller'),('Otros', 'Outros')], string='Sexo',required=True)
+    sexo_traducido = fields.Selection([('Hombre', 'Home'), ('Mujer', 'Muller'), ('Otros', 'Outros')], string='Sexo',required=True)
 
+    @api.depends('alto_en_cms', 'longo_en_cms', 'ancho_en_cms')
+    def _volume(self):
+        for rexistro in self:
+            rexistro.volume = float(rexistro.alto_en_cms) * float(rexistro.longo_en_cms) * float(rexistro.ancho_en_cms)
+
+    @api.onchange('alto_en_cms')
+    def _avisoAlto(self):
+        for rexistro in self:
+            if rexistro.alto_en_cms > 7:
+                rexistro.literal = 'O alto ten un valor posiblemente excesivo %s é maior que 7' % rexistro.alto_en_cms
+            else:
+                rexistro.literal = ""
 
--- models/suceso.py
+++ models/suceso.py
@@ -4,8 +4,8 @@ from odoo import models, fields, api
 
 
 class suceso(models.Model):
-    _name = 'odoo_basico.informacion'
+    _name = 'odoo_basico.suceso'
     _description = 'Este es el modulo'
     name = fields.Char(required=True, size=20, string="Suceso")
-    descripcion = fields.Text(string="A Descripción do Suceso")  # string é a etiqueta do campo
+    descripcion = fields.Text(string="A Descripción do Suceso")
     nivel = fields.Selection([('Baixo', 'Baixo'), ('Medio', 'Medio'), ('Alto', 'Alto')], string='Nivel')
